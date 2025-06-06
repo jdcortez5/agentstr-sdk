43128e65039dd51c0a05abbb26e7b2452b37628d
Jose Cortez <you@example.com>
2025-06-06 16:26:10 -0700

    Updated imports

--- src/agentstr/__init__.py
+++ src/agentstr/__init__.py
@@ -1,5 +1,6 @@
 from pynostr.key import PrivateKey, PublicKey
-from agentstr.a2a import AgentCard, ChatInput, Skill, PriceHandler, default_price_handler
+from agentstr.a2a import AgentCard, ChatInput, Skill
+from agentstr.pricing import PriceHandler, default_price_handler
 from agentstr.logger import get_logger
 from agentstr.nostr_agent_server import NostrAgentServer, NoteFilters
 from agentstr.nostr_client import NostrClient
